714497aa97b209cd04f5d9f5510aa6aec1ade7f4
liruiqin <liruiqin@zzpxx.com>
2020-08-30 22:22:59 +0800

    添加print

--- lib/container/boost_container.dart
+++ lib/container/boost_container.dart
@@ -149,6 +149,7 @@ class BoostContainerState extends NavigatorState {
 
   @override
   Future<bool> maybePop<T extends Object>([T result]) async {
+    print("maybePop maybePop");
     if (routerHistory.isEmpty) {
       FlutterBoost.singleton.closeCurrent();
       return true;
